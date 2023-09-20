5c14f53920ff68e92340bfebcaca5247938743af
Fernando Rocha <beneditofernandor@gmail.com>
2023-09-20 00:08:48 -0300

    Setando Avatar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use Image;
 
 class UserController extends Controller
 {
@@ -78,6 +79,41 @@ class UserController extends Controller
         }
 
         $user->save();
+
+        return $array;
+    }
+
+    public function updateAvatar(Request $request)
+    {
+
+        $array = ['error' => ''];
+        $allowedTypes = ['image/png', 'image/jpeg', 'image/png'];
+
+        $image = $request->file('avatar');
+        if ($image) {
+            if (in_array($image->getClientMimeType(), $allowedTypes)) {
+                $filename = md5(time()) . rand(0, 9999) . '.jpg';
+
+                $destPath = public_path('/media/avatars/');
+
+                $img = Image::make($image->path())
+                    ->fit(200, 200)
+                    ->save($destPath . '/' . $filename);
+
+                $user = User::find($this->loggedUser['id']);
+                $user->avatar = $filename;
+                $user->save();
+
+                $array['url'] = url('/media/avatars/' . $filename);
+            } else {
+                $array['error'] = 'Arquivo não suportado.';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Arquivo não enviado.';
+            return $array;
+        }
+
 
         return $array;
     }
