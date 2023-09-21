87765d6737c00b62c653e015d46b02cbeeb799c0
Fernando Rocha <beneditofernandor@gmail.com>
2023-09-20 22:36:30 -0300

    Comentando no post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Post;
 use App\Postlike;
+use App\PostComment;
 
 class PostController extends Controller
 {
@@ -56,6 +57,33 @@ class PostController extends Controller
 
 
         //2 verificar like
+
+        return $array;
+    }
+
+    public function comment(Request $request, $id)
+    {
+        $array = ['error' => ''];
+
+        $txt = $request->input('txt');
+
+        $postExists = Post::find($id);
+        if ($postExists) {
+            if ($txt) {
+                $newComment = new PostComment();
+                $newComment->id_post = $id;
+                $newComment->id_user = $this->loggedUser['id'];
+                $newComment->created_at = date('Y-m-d H:i:s');
+                $newComment->body = $txt;
+                $newComment->save();
+            } else {
+                $array['error'] = 'Não enviou mensagem.';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Post não existe!';
+            return $array;
+        }
 
         return $array;
     }
